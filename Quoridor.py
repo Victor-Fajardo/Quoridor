3f122834128238257f558925fa8364d41973e22b
Victor-Fajardo <51037722+Victor-Fajardo@users.noreply.github.com>
2020-11-27 11:37:34 -0500

    Bot and player functions implemented

--- Quoridor.py
+++ Quoridor.py
@@ -2,6 +2,9 @@ import pygame
 import networkx
 import matplotlib.pyplot as plt
 from collections import deque
+import random as rd
+from findingPaths import *
+
 
 #Board Size Defined:
 global rows
@@ -15,25 +18,6 @@ Board_Graph = networkx.Graph()
 Board_Graph.add_nodes_from((i, j) for i in range(rows) for j in range(columns))
 Board_Graph.add_edges_from((((i, j), (i - 1, j)) for i in range(rows) for j in range(columns) if i > 0))
 Board_Graph.add_edges_from((((i, j), (i, j - 1)) for i in range(rows) for j in range(columns) if j > 0))
-
-#Creating a wall will delete the edge
-def RemoveEdge(x0, y0, x1, y1):
-	Board_Graph.remove_edge((x0, y0), (x1, y1))
-	return 0
-
-'''
-RemoveEdge(0,0,0,1)
-RemoveEdge(0,2,1,2)
-RemoveEdge(7,5,8,5)
-RemoveEdge(4,3,4,4)
-RemoveEdge(0,6,1,6)
-RemoveEdge(2,0,3,0)
-'''
-#ToDo:
-#Algorithm to place walls 
-#def PlaceWall():
-	#If degree return more than 1 then a wall can be placed
-#	Board_Graph.degree[(0,0)]
 
 def BFS(ini, fin):
 	time_start = pygame.time.get_ticks()
@@ -66,13 +50,16 @@ def BFS(ini, fin):
 				queue.append((nx, ny))
 				#visit[nx][ny] = True
 				dad[(nx, ny)] = cur
-
-	p = reconstructionPath()
+	try:
+		p = reconstructionPath()
+	except Exception as e:
+		return [], 0, False
+	else:
+		time_end = pygame.time.get_ticks()
+		time = time_end - time_start
+		return p, time, True
 	#print(len(p))
-	time_end = pygame.time.get_ticks()
-	time = time_end - time_start
-	return p, time
-
+	
 def DFS(ini, fin):
 	time_start = pygame.time.get_ticks()
 	def invalid(x, y):
@@ -159,12 +146,69 @@ def BruteForce(ini, fin):
 #====================================================#
 #====================================================#
 
+
+
+def performMovement(player_pos, bot_pos):
+    cur_movement = chooseMovement()
+
+    if (cur_movement == 0) and (putUpWalls(player_pos)):
+        updateLastsMovements(cur_movement)
+        return bot_pos
+
+    updateLastsMovements(1)
+    return moveOnTheBoard(bot_pos)
+
+    # Metodo que actualiza el historial de los movimientos del bot.
+def updateLastsMovements(newmovement):
+    last_movements[0] = last_movements[1]
+    last_movements[1] = newmovement
+    return 0
+
+    # Metodo que elige el movimiento a realizar el bot.
+    # 1: moveOnTheBoard, 0: putUpWalls
+def chooseMovement():
+    if (last_movements[1] is None) and (last_movements[0] == last_movements[1]):
+        curmove = 1 if (last_movements[1] == 0) else 1
+    else:
+        curmove = rd.randint(0, 1)
+
+    return curmove
+
+    # Metodo que devuelve si se pudo poner muro en el board.
+def putUpWalls(player_pos):
+    player_path = BFS(player_pos, (8, player_pos[1]))
+
+    if PlaceWall(player_path[0][0], player_path[0][1]):
+        return True
+    return False
+
+    # Metodo que devuelve la posicion nueva del bot.
+def moveOnTheBoard(bot_pos):
+    path = None
+    if IQ == 3:
+        path = BFS(bot_pos, (0,bot_pos[1]))
+    elif IQ == 2:
+        path = BruteForce(bot_pos, (0,bot_pos[1]))
+    elif IQ == 1:
+        path = DFS(bot_pos, (0,bot_pos[1]))
+
+    bot_pos = path[0][1]  # actualizamos la posicion del bot como atributo.
+    return bot_pos
+
+
+#====================================================#
+#====================================================#
+#====================================================#
+#====================================================#
+#====================================================#
+#====================================================#
 #Board visual representation using Pygame
 pygame.init()
 
 #Font created
 pygame.font.init()
 font = pygame.font.SysFont("verdana", 16)
+font2 = pygame.font.SysFont("verdana", 69)
 
 #Screen size defined:
 global width
@@ -173,23 +217,48 @@ width  = 800
 height = 800
 size = (width,height)
 
-start_pos = (0, 0)
-algorithm = 1
-algorithm_name = "BFS"
-path_lenght = 0
-time = 0
-path = None
-
 #Colors defined:
 BLACK    = (   0,   0,   0)
 WHITE    = ( 255, 255, 255)
 GREEN    = (   0, 255,   0)
 RED      = ( 255,   0,   0)
 BLUE     = (   0,   0, 255)
+YELLOW	 = ( 255, 255,   0)
+
+start_pos = (0, 0)
+algorithm = 1
+algorithm_name = "BFS"
+path_lenght = 0
+time = 0
+path = None
+mode = 0
+
+#Game variables
+global game_status
+game_status = True
+game_time = 0
+
+#Player variables
+player_pos = (0,4)
+player_color = BLUE
+player_turn = True
+player_walls = 0
+
+#Bot variables
+bot_pos = (8,4)
+bot_color = GREEN
+bot_walls = 0
+global last_movements
+last_movements = [None, None]
+global IQ
+IQ = 3
 
 #Screen created:
 screen = pygame.display.set_mode(size)
 done = False
+
+#Walls declared:
+walls = []
 
 #Function to draw the board
 def DrawBoard():
@@ -197,10 +266,10 @@ def DrawBoard():
 	SpaceY = int((height-100)/rows)
 	for i in range(columns+1):
 		x = 50 + SpaceX*i
-		pygame.draw.line(screen, BLUE, (x,80), (x,SpaceX*columns+80))
+		pygame.draw.line(screen, WHITE, (x,80), (x,SpaceX*columns+80))
 	for i in range(rows+1):
 		y = 80 + SpaceY*i
-		pygame.draw.line(screen, BLUE, (50,y), (SpaceY*rows+50,y))
+		pygame.draw.line(screen, WHITE, (50,y), (SpaceY*rows+50,y))
 
 def ConvertMousePos(MousePos):
 	SpaceX = int((width-100)/columns)
@@ -239,88 +308,156 @@ def DrawWall(node1, node2):
 		x  = x0
 		y  = 80 + SpaceY*(node2[1]+1)
 
-	pygame.draw.line(screen, WHITE, (x0, y0), (x, y), 5)
+	pygame.draw.line(screen, RED, (x0, y0), (x, y), 5)
 
+#Creating a wall will delete the edge
+def RemoveEdge(start, end):
+	Board_Graph.remove_edge((start[0], start[1]), (end[0], end[1]))
+	return 0
+
+def VerifyWall(start, end):
+	if len(walls) < 20:
+		if BFS(start, (0,0))[2]:
+			if BFS(start, (0,8))[2]:
+				if BFS(start, (8,0))[2]:
+					if BFS(start, (8,8))[2]:
+						if BFS(start, (5,5))[2]:
+							if BFS((0,0), end)[2]:
+								if BFS((0,8), end)[2]:
+									if BFS((8,0), end)[2]:
+										if BFS((8,8), end)[2]:
+											if BFS((4,4), end)[2]:
+												return True
+	return False
+
+def PlaceWall(start, end):
+	if Board_Graph.has_edge(start, end):
+		RemoveEdge(start, end)
+		if VerifyWall(start, end):
+			walls.append((start, end))
+			return True
+		Board_Graph.add_edge(start, end)
+		print("No se puede encerrar un area")
+		return False
+	return False
+
+def VerticalPlacement(MousePos):
+	SpaceY = int((height-100)/rows)
+	y = int((MousePos[1]-80)/SpaceY)
+
+	SpaceX = int((width-100)/columns)
+	x = round((MousePos[0]-50)/SpaceX)
+	aux = int((MousePos[0]-50)/SpaceX)
+
+	if x > aux:
+		x2 = x
+		x = aux
+	elif x == aux:
+		x2 = x
+		x = aux-1
+
+	start = (x, y)
+	end = (x2, y)
+
+	return PlaceWall(start, end)
+
+def HorizontalPlacement(MousePos):
+	SpaceX = int((width-100)/columns)
+	x = int((MousePos[0]-50)/SpaceX)
+
+	SpaceY = int((height-100)/rows)
+	y = round((MousePos[1]-80)/SpaceY)
+	aux = int((MousePos[1]-80)/SpaceY)
+
+	if y > aux:
+		y2 = y
+		y = aux
+	elif y == aux:
+		y2 = y
+		y = aux-1
+
+	start = (x, y)
+	end = (x, y2)
+	
+	return PlaceWall(start, end)
+
+#Player functions:
+def Move(actual, target):
+	if abs(actual[0]-target[0]+actual[1]-target[1]) == 1:
+		if Board_Graph.has_edge(actual, target):
+			return True
+	return False
+
+#Update screen
 def RefreshScreen():
 	screen.blit(instructions1, (0, 0))
 	screen.blit(instructions2, (0, 16))
 	screen.blit(instructions3, (0, 32))
-	screen.blit(alg_name, (width-200, 0))
-	screen.blit(alg_time, (0, height-21))
-	screen.blit(path_len, (width-200, height-21))
-	FillSquare(RED, start_pos)
+	FillSquare(player_color, player_pos)
+	FillSquare(bot_color, bot_pos)
 	DrawBoard()
-	'''
-	DrawWall((0,0), (0,1))
-	DrawWall((0,2), (1,2))
-	DrawWall((7,5), (8,5))
-	DrawWall((4,3), (4,4))
-	DrawWall((0,6), (1,6))
-	DrawWall((2,0), (3,0))
-	'''
+
+	for i in walls:
+		DrawWall(i[0], i[1])
+
+	if not game_status:
+		screen.blit(game_over, (200, height-500))
+		screen.blit(winner, (200, height-450))
 	pygame.display.flip()
 
 #Game Loop:
 while not done:
+
+	#Win condition
+	if player_pos[0] == 8 or bot_pos[0] == 0:
+		game_status = False
+
+	if not player_turn:
+			if game_status:
+				pygame.time.delay(500)
+				bot_pos = performMovement(player_pos, bot_pos)
+				player_turn = True
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: 
 			done = True
-		#Clicking a tile will create a path to it from (0, 0)
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			if event.button == 1:
-				pos = ConvertMousePos(pygame.mouse.get_pos())
-				start_pos = pos
-				screen.fill(BLACK)
-				path = ()
 
-			if event.button == 3:
-				pos = ConvertMousePos(pygame.mouse.get_pos())
+		if player_turn:
+			#Clicking a tile will create a path to it from (0, 0)
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button == 1:
+					if mode == 0:
+						mouse_pos = ConvertMousePos(pygame.mouse.get_pos())
+						if Move(player_pos, mouse_pos):
+							player_pos = mouse_pos
+							screen.fill(BLACK)
+							player_turn = False
+					if mode == 1:
+						if VerticalPlacement(pygame.mouse.get_pos()):
+							player_turn = False
+					if mode == 2:
+						if HorizontalPlacement(pygame.mouse.get_pos()):
+							player_turn = False
 
-				if algorithm == 1:
-					alg_result = BFS(start_pos, pos)
-				elif algorithm == 2:
-					alg_result = DFS(start_pos, pos)
-				elif algorithm == 3:
-					alg_result = BruteForce(start_pos, pos)
-
-				path = alg_result[0]
-				path_lenght = len(path)
-				time = alg_result[1]
-				screen.fill(BLACK)
-				for i in range(len(path)):
-					FillSquare(GREEN, path[i])
-					#Delay added
-					pygame.time.delay(50)
-					RefreshScreen()
-
-
-		#Algorithm selection
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_1:
-				algorithm = 1
-				algorithm_name = "BFS"
-				screen.fill(BLACK)
-			if event.key == pygame.K_2:
-				algorithm = 2
-				algorithm_name = "DFS"
-				screen.fill(BLACK)
-			if event.key == pygame.K_3:
-				algorithm = 3
-				algorithm_name = "Fuerza Bruta"
-				screen.fill(BLACK)
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_q:
+					mode = 0
+				if event.key == pygame.K_w:
+					mode = 1
+				if event.key == pygame.K_e:
+					mode = 2
 
 	if path != None:
 		if len(path) > 0:
 			start_pos = path[0]
 			path.popleft()
-
+	
 	screen.fill(BLACK)
 	instructions1 = font.render("Click izquierdo -> Seleccionar punto de inicio", True, WHITE)
 	instructions2 = font.render("Click derecho -> Generar camino", True, WHITE)
 	instructions3 = font.render("1, 2 y 3-> Cambiar de algoritmo", True, WHITE)
-	alg_name = font.render("Algoritmo: " + str(algorithm_name), True, WHITE)
-	alg_time = font.render("Tiempo de ejecucion: "+ str(time) + "ms", True, WHITE)
-	path_len = font.render("Nodos recorridos: " + str(path_lenght), True, WHITE)
-	pygame.time.delay(50)
+	game_over = font2.render("Game Over", True, RED)
+	winner = font2.render("Ha Ganado", True, RED)
+	pygame.time.delay(30)
 	DrawPath(path)
 	RefreshScreen()
